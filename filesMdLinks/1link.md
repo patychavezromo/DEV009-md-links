d5161f9bd46132b144415956491d4063ef15b6b8
Paty Chavez <patricia.araceli.chavez.romo@gmail.com>
2023-09-26 11:49:00 -0600

    Se agregan las funciones para hacer peticiones get por el protocolo http y obtener el status code y status ok

---
+++ filesMdLinks/1link.md
@@ -0,0 +1,1 @@
+* [Array.prototype.sort() - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/sorts)
--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const data = require("./data");
 
-const mdLinks = (route) =>{
+const mdLinks = (path, validate) =>{
     return new Promise((resolve, reject) => {
-        const absoluteRoute = data.toAbsolute(route);
+        const absoluteRoute = data.toAbsolute(path);
         const routeExists = data.existsTheRoute(absoluteRoute);
         if(!routeExists){
             reject (new Error('la ruta no existe'));
             return;
         }
-        const isMarkdownTheFileExtension = data.isMarkdownTheFileExtension(route);
+        const isMarkdownTheFileExtension = data.isMarkdownTheFileExtension(path);
         if(!isMarkdownTheFileExtension){
             reject (new Error('la extensión del archivo no es de tipo MarkDown'));
             return;
         }
-        return data.getDataFromFile(route)
+        return data.getDataFromFile(path, validate)
             .then(data => resolve(data))
             .catch(error => reject(error));             
     });
 }
 
-mdLinks('/Users/patri/Desktop/proyectos_laboratoria/proyecto4/mdlinks/DEV009-md-links/filesMdLinks/mdFile.md')
+// mdLinks('/Users/patri/Desktop/proyectos_laboratoria/proyecto4/mdlinks/DEV009-md-links/filesMdLinks/mdFile.md')
+
+mdLinks('./filesMdLinks/1link.md',true)
     .then(result => {
         console.log(result);
     }).catch ((error) => {
